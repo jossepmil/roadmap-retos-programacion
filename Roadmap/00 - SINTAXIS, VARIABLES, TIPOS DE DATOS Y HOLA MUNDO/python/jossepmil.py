6ee212c1ccd63ce74c84b02f47c688a4b3f7f6c1
Emiliano Tejo <58150343+jossepmil@users.noreply.github.com>
2024-04-21 12:46:44 -0400

    jossepmil.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jossepmil.py
@@ -0,0 +1,26 @@
+# https://www.python.org
+
+"""
+un comentario de varias lineas
+""""
+#comentario de una sola linea
+minombre = "Emiliano"
+MI_CONSTANTE = 100
+
+flotante = float("3.14")
+entero = int(2)
+cadena = str("hola mundo")
+booleano = True
+tupla = (1, 2, 3)
+lista = [1, 2, 3]
+diccionario = {"nombre": "Emiliano", "edad": 25}
+
+# operadores aritméticos
+suma = 1 + 2
+resta = 4 - 3
+multiplicacion = 6 * 7
+division = 8 / 2
+potencia = 5 ** 2
+modulo = 9 % 3
+
+print(¡Hola, python!)
